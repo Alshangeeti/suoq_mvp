ca91e691109883b37fbaf792319e9ea3372c8863
Alshangeeti <114350340+Alshangeeti@users.noreply.github.com>
2026-07-07 23:13:24 +0300

    Delete souq-mauritania-mvp/souq-mvp directory

--- souq-mauritania-mvp/souq-mvp/app/api/products/route.js
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-import { prisma } from "../../../lib/db";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const products = await prisma.product.findMany({ orderBy: { id: "asc" } });
-  return NextResponse.json(products);
-}
